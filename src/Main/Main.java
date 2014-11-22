2e74ef6f79dd86bb8c61135a086c5f3747baf63f
unknown <brett.schnobrich@duke.edu>
2014-11-22 16:18:34 -0500

    writing test

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -23,11 +23,23 @@ public class Main {
 		globalDFS.init();
 		globalTestEventBarrier = new EventBarrier();
 		
-		testCreateFile();
+		//testCreateFile();
+		testWrite();
 		
 		globalTestEventBarrier.arrive(); // wait until everything has tested
 		
 		printOutFSState();
+	}
+	
+	private static void testWrite() {
+		User u0 = new User(null, null, 0, 0, Constants.DiskOperationType.CREATE);
+		u0.start();
+		byte[] buffer = {'h','e','l','l','o'};
+		byte[] buff = new byte[8];
+		User u1 = new User(((LocalDFS)globalDFS).getDFileID(0), buffer, 1, 4, Constants.DiskOperationType.WRITE);
+		User u2 = new User(((LocalDFS)globalDFS).getDFileID(0), buff, 1, 4, Constants.DiskOperationType.READ);
+		u1.start();
+		u2.start();
 	}
 	
 	private static void testCreateFile() {
